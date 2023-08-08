81de3963607ed5ab2b48dc67a52d9faa53d952c3
Hernan <calderon.hernanb@gmail.com>
2023-08-08 12:43:50 -0600

    Good byeee

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -1,9 +1,9 @@
 export default function Terms() {
     return (
-        <div className="text-white px-4">
+        <div className='text-white px-4'>
             <h1 className='text-3xl'>Términos de Servicio</h1>            
 
-            <p>Estos términos y condiciones ("Términos de Servicio") rigen el uso de <strong>Rythmo</strong> ("nosotros", "nuestro"). Al acceder a nuestro sitio y usar nuestros servicios, aceptas estos Términos de Servicio.</p>
+            <p>Estos términos y condiciones (Términos de Servicio) rigen el uso de <strong>Rythmo</strong> (nosotros, nuestro). Al acceder a nuestro sitio y usar nuestros servicios, aceptas estos Términos de Servicio.</p>
 
             <h2>1. Uso del Servicio</h2>
             <p>Se límita únicamente al uso academico.
